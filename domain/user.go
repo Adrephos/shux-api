f54c8337837b7f8db4d4f4909a9a6da56082b0e2
Adrephos <adrephos@gmail.com>
2022-12-19 15:47:36 -0500

    feat: define channel struct and repository

---
+++ domain/user.go
@@ -0,0 +1,28 @@
+package domain
+
+import (
+	"time"
+)
+
+type (
+	UserRepository interface {
+		FindUser(string) (User, error)
+		DeleteUser(string) (User, error)
+		UpdateUser(User) (User, error)
+		ReplaceUser(User) (User, error)
+	}
+	warning struct{
+		Reason		   string		`json:"reason"`
+		Date		   time.Time	`json:"date"`
+	}
+	User struct {
+		UserId         string		`json:"id"`
+		Description    string		`json:"description"`
+		Points         int64		`json:"points"`
+		ShowLevel      bool			`json:"show_level"`
+		Warnings       int64		`json:"warnings"`
+		WarningsRecord []warning	`json:"warnings_record"`
+		Beta           bool			`json:"beta"`
+		Tickets        int64		`json:"tickets"`
+	}
+)
