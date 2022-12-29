e292097c83686400ebfaa66a09117e99aac8a09a
Adrephos <adrephos@gmail.com>
2022-12-29 12:10:07 -0500

    feat: finish CRUD functions for firestore documents

---
+++ infrastructure/persistance/firestore.go
@@ -0,0 +1,104 @@
+package persistance
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	firestore "cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+var Client *firestore.Client
+var ctx = context.Background()
+var app *firebase.App
+
+// fuction to intilize the firestore client
+func init() {
+	var err error
+	sapath, _ := os.LookupEnv("SHUX_API_SA")
+
+	sa := option.WithCredentialsFile(sapath)
+	app, err = firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	Client, err = app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// function that returns a map with the data of a firestore doc
+func Get(path string) (map[string]interface{}, error) {
+	data := make(map[string]interface{})
+	var err error
+
+	doc, err := Client.Doc(path).Get(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	data = doc.Data()
+	docParent := doc.Ref.Parent.ID[:len(doc.Ref.Parent.ID)-1]
+	id := fmt.Sprintf("%v_id", docParent)
+	data[id] = doc.Ref.ID
+
+	fmt.Println("Found document with id:", doc.Ref.ID)
+
+	return data, err
+}
+
+// function to delete a firestore document in a given path
+func Delete(path string, id string) error {
+	docRef := Client.Collection(path).Doc(id)
+	_, err := docRef.Delete(ctx)
+	if err != nil{
+		fmt.Println(err)
+		return err
+	}
+
+	fmt.Println("Deleted document with ID:", docRef.ID)
+	return nil
+}
+
+// function to create a doc in firestore in a given path
+func Create(path string, data interface{}, id string) error{
+	docRef := Client.Collection(path).Doc(id)
+	_, err := docRef.Set(ctx, data)
+    if err != nil {
+		fmt.Println(err)
+        return err
+    }
+    fmt.Println("Created document with ID:", docRef.ID)
+    return nil
+}
+
+// functions to update a existing doc with its id and path
+func Update(path string, data interface{}, id string) error{
+	dataJson, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
+        return err
+	}
+
+	var dataMap map[string]interface{}
+    err = json.Unmarshal(dataJson, &dataMap)
+    if err != nil {
+        return err
+    }
+
+	docRef := Client.Collection(path).Doc(id)
+	_, err = docRef.Set(ctx, dataMap, firestore.MergeAll)
+    if err != nil {
+		fmt.Println(err)
+        return err
+    }
+    fmt.Println("Updated document with ID:", docRef.ID)
+    return nil
+}
