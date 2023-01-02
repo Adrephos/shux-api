d6d8d2da3e471b48dbc5ca18064ae02076fba90f
Adrephos <adrephos@gmail.com>
2023-01-02 17:01:54 -0500

    feat: start definition and implementation of interface functions

---
+++ application/user.go
@@ -0,0 +1,41 @@
+package application
+
+import (
+	"fmt"
+	"github.com/shuxbot/shux-api/domain"
+)
+
+type UserApp struct{
+	UserRepo domain.UserRepository
+}
+
+func (app *UserApp) Get(UserId string, ServerId string) (domain.User, error){
+	user, err := app.UserRepo.Get(UserId, ServerId)
+	fmt.Println(user)
+
+	return user, err
+}
+
+func (app *UserApp) Delete(UserId string, ServerId string) error {
+	err := app.UserRepo.Delete(UserId, ServerId)
+
+	return err
+}
+
+func (app *UserApp) Update(u *domain.User, ServerId string) error{
+	err := app.UserRepo.Update(u, ServerId)
+
+	return err
+}
+
+func (app *UserApp) Create(u *domain.User, ServerId string) error{
+	err := app.UserRepo.Create(u, ServerId)
+
+	return err
+}
+
+
+
+func NewUserRepo(userRepo domain.UserRepository) *UserApp {
+	return &UserApp{UserRepo: userRepo}
+}
