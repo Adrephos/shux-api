a5b1f817ca78132010a98ba6173cdeddb5e823af
Adrephos <adrephos@gmail.com>
2023-01-03 21:55:18 -0500

    feat: add channel repo and a function to handle a channel endpoint

--- infrastructure/routeHandler.go
+++ infrastructure/routeHandler.go
@@ -7,9 +7,17 @@ import (
 	"github.com/shuxbot/shux-api/domain"
 )
 
-
 type routeHandler struct {
 	userApp *application.UserApp
+	channelApp *application.ChannelApp
+}
+
+func bodyToUserStruct(c *fiber.Ctx) domain.User {
+	var u domain.User
+	json.Unmarshal(c.Body(), &u)
+	u.UserId = c.Params("user_id")
+
+	return u
 }
 
 func result(success bool, err error) map[string]interface{} {
@@ -36,20 +44,40 @@ func (h *routeHandler) DeleteUser(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil))
 }
 
-func (h *routeHandler) CreateUser(c *fiber.Ctx) error {
-	var u domain.User
-	json.Unmarshal(c.Body(), &u)
-	u.UserId = c.Params("user_id")
+func (h *routeHandler) UpdateUser(c *fiber.Ctx) error {
+	u := bodyToUserStruct(c)
+	err := h.userApp.Update(&u, c.Params("server_id"))
 
+	if err != nil {
+		return c.Status(404).JSON(result(false, err))
+	}
+	return c.JSON(result(true, nil))
+
+}
+
+func (h *routeHandler) CreateUser(c *fiber.Ctx) error {
+	u := bodyToUserStruct(c)
 	err := h.userApp.Create(&u, c.Params("server_id"))
 
 	if err != nil {
 		return c.Status(404).JSON(result(false, err))
 	}
-
 	return c.JSON(result(true, nil))
 }
 
-func NewRouteHandler(userApp *application.UserApp) *routeHandler {
-	return &routeHandler{userApp: userApp}
+
+func (h *routeHandler) ListChannels(c *fiber.Ctx) error {
+	cMap := make(map[string]interface{})
+	cArr, err := h.channelApp.List(c.Params("server_id"))
+	cMap["channels"] = cArr
+
+	if err != nil {
+		return c.Status(404).JSON(result(false, err))
+	}
+
+	return c.JSON(cMap)
+}
+
+func NewRouteHandler(userApp *application.UserApp, channelApp *application.ChannelApp) *routeHandler {
+	return &routeHandler{userApp: userApp, channelApp: channelApp}
 }
