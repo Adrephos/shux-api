6e5c4b5c6fea181a0aa6f54006d667eb18841b80
Adrephos <adrephos@gmail.com>
2023-01-03 22:12:12 -0500

    feat: add a channel endpoint for testing

--- main.go
+++ main.go
@@ -11,14 +11,22 @@ func main() {
 
 	firestoreClient := persistance.Client
 	userRepo := infrastructure.NewFirestoreUserRepo(firestoreClient)
-	userApp := application.NewUserRepo(userRepo)
-	routeHandler := infrastructure.NewRouteHandler(userApp)
+	channelRepo := infrastructure.NewFirestoreChannelRepo(firestoreClient)
+	userApp := application.NewUserApp(userRepo)
+	channelApp := application.NewChannelApp(channelRepo)
+	routeHandler := infrastructure.NewRouteHandler(userApp, channelApp)
 
 	app := fiber.New()
 
+	//User endpoints
 	app.Get("/servers/:server_id/users/:user_id", routeHandler.GetUser)
 	app.Delete("/servers/:server_id/users/:user_id", routeHandler.DeleteUser)
+	app.Patch("/servers/:server_id/users/:user_id", routeHandler.UpdateUser)
+	app.Put("/servers/:server_id/users/:user_id", routeHandler.CreateUser)
 	app.Post("/servers/:server_id/users/:user_id", routeHandler.CreateUser)
+
+	//Channel endpoints
+	app.Get("/servers/:server_id/channels", routeHandler.ListChannels)
 
 	app.Listen(":3000")
 }
