08cc8e72b680f8a92c4db6a6064ecfe83a59f73c
Adrephos <adrephos@gmail.com>
2023-01-03 22:11:37 -0500

    feat: change how IDs are encoded in json

--- domain/channel.go
+++ domain/channel.go
@@ -2,15 +2,15 @@ package domain
 
 type (
 	ChannelRepository interface {
-		List() ([]Channel, error)
-		Get(id string) (Channel, error)
-		Delete(id string) (Channel, error)
-		Update(c *Channel) error
-		Replace(c *Channel) error
-		Create(c *Channel) error
+		List(ServerId string) ([]Channel, error)
+		Get(ChannelId string, ServerId string) (Channel, error)
+		Delete(ChannelId string, ServerId string) error
+		Update(c *Channel, ServerId string) error
+		Replace(c *Channel, ServerId string) error
+		Create(c *Channel, ServerId string) error
 	}
 	Channel struct {
-		ChannelId	   string		`json:"channel_id,omitempty" firestore:"-"`
+		ChannelId	   string		`json:"id,omitempty" firestore:"-"`
 		Flags		   int64		`json:"flags,omitempty" firestore:"flags,omitempty"`
 	}
 )
--- domain/user.go
+++ domain/user.go
@@ -12,17 +12,17 @@ type (
 		Replace(u *User, ServerId string) error
 		Create(u *User, ServerId string) error
 	}
-	Warning struct{
-		Reason		   string		`json:"reason"`
-		Date		   time.Time	`json:"date"`
+	Warning struct {
+		Reason string    `json:"reason"`
+		Date   time.Time `json:"date"`
 	}
 	User struct {
-		UserId         string		`json:"user_id,omitempty" firestore:"-"`
-		Description    string		`json:"description,omitempty" firestore:"description,omitempty"`
-		Points         int64		`json:"points,omitempty" firestore:"points,omitempty"`
-		Warnings       int64		`json:"warnings,omitempty" firestore:"warnings,omitempty"`
-		WarningsRecord []Warning	`json:"warnings_record,omitempty" firestore:"warnings_record,omitempty"`
-		Tickets        int64		`json:"tickets,omitempty" firestore:"tickets,omitempty"`
-		Flags		   int64		`json:"flags,omitempty" firestore:"flags,omitempty"`
+		UserId         string    `json:"id,omitempty" firestore:"-"`
+		Description    string    `json:"description,omitempty" firestore:"description,omitempty"`
+		Points         int64     `json:"points,omitempty" firestore:"points,omitempty"`
+		Warnings       int64     `json:"warnings,omitempty" firestore:"warnings,omitempty"`
+		WarningsRecord []Warning `json:"warnings_record,omitempty" firestore:"warnings_record,omitempty"`
+		Tickets        int64     `json:"tickets,omitempty" firestore:"tickets,omitempty"`
+		Flags          int64     `json:"flags,omitempty" firestore:"flags,omitempty"`
 	}
 )
