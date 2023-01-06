e1f100d194771ec865d1d80b89a70ff984775c32
Adrephos <adrephos@gmail.com>
2023-01-06 13:03:34 -0500

    feat: add ServerId as parameter in role method

--- domain/role.go
+++ domain/role.go
@@ -3,11 +3,11 @@ package domain
 type (
 	RoleRepository interface {
 		List() ([]Role, error)
-		Get(id string) error
-		Delete(id string) error
-		Update(r *Role) error
-		Replace(r *Role) error
-		Create(r *Role) error
+		Get(RoleId string, ServerId string) (Role, error)
+		Delete(RoleId string, ServerId string) error
+		Update(r *Role, ServerId string) error
+		Replace(r *Role, ServerId string) error
+		Create(r *Role, ServerId string) error
 	}
 	Role struct {
 		RoleId		string		`json:"role_id,omitempty" firestore:"-"`
