e1eb22c30372957182bbef67f1667e64d60aa403
Adrephos <adrephos@gmail.com>
2023-01-25 16:53:04 -0500

    feat: add cache handlers to endpoints

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/shuxbot/shux-api/application"
 	"github.com/shuxbot/shux-api/infrastructure"
 	"github.com/shuxbot/shux-api/infrastructure/persistance"
+	"github.com/shuxbot/shux-api/infrastructure/transient"
+	"github.com/goccy/go-json"
 )
 
 func main() {
@@ -21,35 +24,40 @@ func main() {
 	serverApp := application.NewServerApp(serverRepo)
 	routeHandler := infrastructure.NewRouteHandler(userApp, channelApp, roleApp, serverApp)
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+        JSONEncoder: json.Marshal,
+        JSONDecoder: json.Unmarshal,
+    })
+
+	app.Use(logger.New())
 
 	//Server endpoints
-	app.Get("/servers", routeHandler.ListServers)
-	app.Get("/servers/:server_id/ranking", routeHandler.ServerRanking)
-	app.Get("/servers/:server_id/ranking/:user_id", routeHandler.ServerUserRanking)
+	app.Get("/servers", transient.New(false), routeHandler.ListServers)
+	app.Get("/servers/:server_id/ranking", transient.New(false), routeHandler.ServerRanking)
+	app.Get("/servers/:server_id/ranking/:user_id", transient.New(false), routeHandler.ServerUserRanking)
 
 	//User endpoints
-	app.Get("/servers/:server_id/users/:user_id", routeHandler.GetUser)
-	app.Delete("/servers/:server_id/users/:user_id", routeHandler.DeleteUser)
-	app.Patch("/servers/:server_id/users/:user_id", routeHandler.UpdateUser)
-	app.Put("/servers/:server_id/users/:user_id", routeHandler.ReplaceUser)
-	app.Post("/servers/:server_id/users/:user_id", routeHandler.CreateUser)
+	app.Get("/servers/:server_id/users/:user_id", transient.New(true), routeHandler.GetUser)
+	app.Delete("/servers/:server_id/users/:user_id", transient.New(true), routeHandler.DeleteUser)
+	app.Patch("/servers/:server_id/users/:user_id", transient.New(true), routeHandler.UpdateUser)
+	app.Put("/servers/:server_id/users/:user_id", transient.New(true), routeHandler.ReplaceUser)
+	app.Post("/servers/:server_id/users/:user_id", transient.New(true), routeHandler.CreateUser)
 
 	//Channel endpoints
-	app.Get("/servers/:server_id/channels", routeHandler.ListChannels)
-	app.Get("/servers/:server_id/channels/:channel_id", routeHandler.GetChannel)
-	app.Delete("/servers/:server_id/channels/:channel_id", routeHandler.DeleteChannel)
-	app.Patch("/servers/:server_id/channels/:channel_id", routeHandler.UpdateChannel)
-	app.Put("/servers/:server_id/channels/:channel_id", routeHandler.ReplaceChannel)
-	app.Post("/servers/:server_id/channels/:channel_id", routeHandler.CreateChannel)
+	app.Get("/servers/:server_id/channels", transient.New(false), routeHandler.ListChannels)
+	app.Get("/servers/:server_id/channels/:channel_id", transient.New(true), routeHandler.GetChannel)
+	app.Delete("/servers/:server_id/channels/:channel_id", transient.New(true), routeHandler.DeleteChannel)
+	app.Patch("/servers/:server_id/channels/:channel_id", transient.New(true), routeHandler.UpdateChannel)
+	app.Put("/servers/:server_id/channels/:channel_id", transient.New(true), routeHandler.ReplaceChannel)
+	app.Post("/servers/:server_id/channels/:channel_id", transient.New(true), routeHandler.CreateChannel)
 
 	//Role endpoints
-	app.Get("/servers/:server_id/roles", routeHandler.ListRoles)
-	app.Get("/servers/:server_id/roles/:role_id", routeHandler.GetRole)
-	app.Delete("/servers/:server_id/roles/:role_id", routeHandler.DeleteRole)
-	app.Patch("/servers/:server_id/roles/:role_id", routeHandler.UpdateRole)
-	app.Put("/servers/:server_id/roles/:role_id", routeHandler.ReplaceRole)
-	app.Post("/servers/:server_id/roles/:role_id", routeHandler.CreateRole)
+	app.Get("/servers/:server_id/roles", transient.New(false), routeHandler.ListRoles)
+	app.Get("/servers/:server_id/roles/:role_id", transient.New(true), routeHandler.GetRole)
+	app.Delete("/servers/:server_id/roles/:role_id", transient.New(true), routeHandler.DeleteRole)
+	app.Patch("/servers/:server_id/roles/:role_id", transient.New(true), routeHandler.UpdateRole)
+	app.Put("/servers/:server_id/roles/:role_id", transient.New(true), routeHandler.ReplaceRole)
+	app.Post("/servers/:server_id/roles/:role_id", transient.New(true), routeHandler.CreateRole)
 
 	app.Listen(":3000")
 }
