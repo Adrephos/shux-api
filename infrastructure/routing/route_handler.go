b82a8b897ac54ef8740429b69c1a3f42eb339f15
Adrephos <adrephos@gmail.com>
2023-10-14 14:30:48 -0500

    style: move routeHandler to its own folder

--- infrastructure/routing/route_handler.go
+++ infrastructure/routing/route_handler.go
@@ -1,4 +1,4 @@
-package infrastructure
+package routing
 
 import (
 	"github.com/goccy/go-json"
@@ -8,7 +8,7 @@ import (
 	"github.com/shuxbot/shux-api/domain"
 )
 
-type routeHandler struct {
+type RouteHandler struct {
 	userApp    *application.UserApp
 	channelApp *application.ChannelApp
 	roleApp    *application.RoleApp
@@ -51,7 +51,8 @@ func result(success bool, err error, data interface{}) map[string]interface{} {
 	return status
 }
 
-func (h *routeHandler) GetUser(c *fiber.Ctx) error {
+// User endpoints
+func (h *RouteHandler) GetUser(c *fiber.Ctx) error {
 	u, err := h.userApp.Get(c.Params("user_id"), c.Params("server_id"))
 	if err != nil {
 		return c.Status(404).JSON(result(false, err, nil))
@@ -59,7 +60,7 @@ func (h *routeHandler) GetUser(c *fiber.Ctx) error {
 	return c.JSON(result(true, err, u))
 }
 
-func (h *routeHandler) DeleteUser(c *fiber.Ctx) error {
+func (h *RouteHandler) DeleteUser(c *fiber.Ctx) error {
 	err := h.userApp.Delete(c.Params("user_id"), c.Params("server_id"))
 	if err != nil {
 		return c.Status(404).JSON(result(false, err, nil))
@@ -67,7 +68,7 @@ func (h *routeHandler) DeleteUser(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, c.Params("user_id")))
 }
 
-func (h *routeHandler) UpdateUser(c *fiber.Ctx) error {
+func (h *RouteHandler) UpdateUser(c *fiber.Ctx) error {
 	u := bodyToUserStruct(c)
 	err := h.userApp.Update(&u, c.Params("server_id"))
 
@@ -78,7 +79,7 @@ func (h *routeHandler) UpdateUser(c *fiber.Ctx) error {
 
 }
 
-func (h *routeHandler) ReplaceUser(c *fiber.Ctx) error {
+func (h *RouteHandler) ReplaceUser(c *fiber.Ctx) error {
 	u := bodyToUserStruct(c)
 	err := h.userApp.Replace(&u, c.Params("server_id"))
 
@@ -88,7 +89,7 @@ func (h *routeHandler) ReplaceUser(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, u))
 }
 
-func (h *routeHandler) CreateUser(c *fiber.Ctx) error {
+func (h *RouteHandler) CreateUser(c *fiber.Ctx) error {
 	u := bodyToUserStruct(c)
 	err := h.userApp.Create(&u, c.Params("server_id"))
 
@@ -98,7 +99,8 @@ func (h *routeHandler) CreateUser(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, u))
 }
 
-func (h *routeHandler) ListChannels(c *fiber.Ctx) error {
+// Channel endpoints
+func (h *RouteHandler) ListChannels(c *fiber.Ctx) error {
 	cMap := make(map[string]interface{})
 	cArr, err := h.channelApp.List(c.Params("server_id"))
 	cMap["channels"] = cArr
@@ -110,7 +112,7 @@ func (h *routeHandler) ListChannels(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, cMap))
 }
 
-func (h *routeHandler) GetChannel(c *fiber.Ctx) error {
+func (h *RouteHandler) GetChannel(c *fiber.Ctx) error {
 	ch, err := h.channelApp.Get(c.Params("channel_id"), c.Params("server_id"))
 	if err != nil {
 		return c.Status(404).JSON(result(false, err, nil))
@@ -118,7 +120,7 @@ func (h *routeHandler) GetChannel(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, ch))
 }
 
-func (h *routeHandler) DeleteChannel(c *fiber.Ctx) error {
+func (h *RouteHandler) DeleteChannel(c *fiber.Ctx) error {
 	err := h.channelApp.Delete(c.Params("channel_id"), c.Params("server_id"))
 	if err != nil {
 		return c.Status(404).JSON(result(false, err, nil))
@@ -126,7 +128,7 @@ func (h *routeHandler) DeleteChannel(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, c.Params("channel_id")))
 }
 
-func (h *routeHandler) UpdateChannel(c *fiber.Ctx) error {
+func (h *RouteHandler) UpdateChannel(c *fiber.Ctx) error {
 	ch := bodyToChannelStruct(c)
 	err := h.channelApp.Update(&ch, c.Params("server_id"))
 
@@ -137,7 +139,7 @@ func (h *routeHandler) UpdateChannel(c *fiber.Ctx) error {
 
 }
 
-func (h *routeHandler) ReplaceChannel(c *fiber.Ctx) error {
+func (h *RouteHandler) ReplaceChannel(c *fiber.Ctx) error {
 	ch := bodyToChannelStruct(c)
 	err := h.channelApp.Replace(&ch, c.Params("server_id"))
 
@@ -147,7 +149,7 @@ func (h *routeHandler) ReplaceChannel(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, ch))
 }
 
-func (h *routeHandler) CreateChannel(c *fiber.Ctx) error {
+func (h *RouteHandler) CreateChannel(c *fiber.Ctx) error {
 	ch := bodyToChannelStruct(c)
 	err := h.channelApp.Create(&ch, c.Params("server_id"))
 
@@ -157,7 +159,8 @@ func (h *routeHandler) CreateChannel(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, ch))
 }
 
-func (h *routeHandler) ListRoles(c *fiber.Ctx) error {
+// Role endpoints
+func (h *RouteHandler) ListRoles(c *fiber.Ctx) error {
 	rlMap := make(map[string]interface{})
 	rlArr, err := h.roleApp.List(c.Params("server_id"))
 	rlMap["roles"] = rlArr
@@ -169,7 +172,7 @@ func (h *routeHandler) ListRoles(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, rlMap))
 }
 
-func (h *routeHandler) GetRole(c *fiber.Ctx) error {
+func (h *RouteHandler) GetRole(c *fiber.Ctx) error {
 	rl, err := h.roleApp.Get(c.Params("role_id"), c.Params("server_id"))
 	if err != nil {
 		return c.Status(404).JSON(result(false, err, nil))
@@ -177,7 +180,7 @@ func (h *routeHandler) GetRole(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, rl))
 }
 
-func (h *routeHandler) DeleteRole(c *fiber.Ctx) error {
+func (h *RouteHandler) DeleteRole(c *fiber.Ctx) error {
 	err := h.roleApp.Delete(c.Params("role_id"), c.Params("server_id"))
 	if err != nil {
 		return c.Status(404).JSON(result(false, err, nil))
@@ -185,7 +188,7 @@ func (h *routeHandler) DeleteRole(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, c.Params("role_id")))
 }
 
-func (h *routeHandler) UpdateRole(c *fiber.Ctx) error {
+func (h *RouteHandler) UpdateRole(c *fiber.Ctx) error {
 	rl := bodyToRoleStruct(c)
 	err := h.roleApp.Update(&rl, c.Params("server_id"))
 
@@ -196,7 +199,7 @@ func (h *routeHandler) UpdateRole(c *fiber.Ctx) error {
 
 }
 
-func (h *routeHandler) ReplaceRole(c *fiber.Ctx) error {
+func (h *RouteHandler) ReplaceRole(c *fiber.Ctx) error {
 	rl := bodyToRoleStruct(c)
 	err := h.roleApp.Replace(&rl, c.Params("server_id"))
 
@@ -206,7 +209,7 @@ func (h *routeHandler) ReplaceRole(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, rl))
 }
 
-func (h *routeHandler) CreateRole(c *fiber.Ctx) error {
+func (h *RouteHandler) CreateRole(c *fiber.Ctx) error {
 	rl := bodyToRoleStruct(c)
 	err := h.roleApp.Create(&rl, c.Params("server_id"))
 
@@ -216,7 +219,8 @@ func (h *routeHandler) CreateRole(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, rl))
 }
 
-func (h *routeHandler) ListServers(c *fiber.Ctx) error {
+// Server endpoints
+func (h *RouteHandler) ListServers(c *fiber.Ctx) error {
 	mapId := make(map[string]interface{})
 	idArr,err := h.serverApp.List()
 	mapId["servers_id"] = idArr
@@ -227,7 +231,7 @@ func (h *routeHandler) ListServers(c *fiber.Ctx) error {
 	return c.JSON(result(true, nil, mapId))
 }
 
-func (h *routeHandler) ServerRanking(c *fiber.Ctx) error{
+func (h *RouteHandler) ServerRanking(c *fiber.Ctx) error{
 	ranking := make(map[string]interface{})
 	serverRanking, err := h.serverApp.GetRanking(c.Params("server_id"))
 	ranking["ranking"] = serverRanking
@@ -239,7 +243,7 @@ func (h *routeHandler) ServerRanking(c *fiber.Ctx) error{
 
 }
 
-func (h *routeHandler) ServerUserRanking(c *fiber.Ctx) error{
+func (h *RouteHandler) ServerUserRanking(c *fiber.Ctx) error{
 	ranking := make(map[string]interface{})
 	userRank, err := h.serverApp.GetUserRank(c.Params("server_id"), c.Params("user_id"))
 	ranking["user"] = userRank
@@ -251,6 +255,6 @@ func (h *routeHandler) ServerUserRanking(c *fiber.Ctx) error{
 
 }
 
-func NewRouteHandler(userApp *application.UserApp, channelApp *application.ChannelApp, roleApp *application.RoleApp, serverApp *application.ServerApp) *routeHandler {
-	return &routeHandler{userApp: userApp, channelApp: channelApp, roleApp: roleApp, serverApp: serverApp}
+func NewRouteHandler(userApp *application.UserApp, channelApp *application.ChannelApp, roleApp *application.RoleApp, serverApp *application.ServerApp) *RouteHandler {
+	return &RouteHandler{userApp: userApp, channelApp: channelApp, roleApp: roleApp, serverApp: serverApp}
 }
