f13658b6e24b389c2e562ae963a23d374ee7e2fe
Adrephos <adrephos@gmail.com>
2023-11-30 18:03:40 -0500

    feat: add jwt implementation for access token and refresh token

---
+++ auth/jwt.go
@@ -0,0 +1,68 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/patrickmn/go-cache"
+)
+
+// Cache for storing refresh tokens
+var RefreshCache = cache.New(20*time.Minute, 30*time.Minute)
+
+func GenerateAccessToken() (string, error) {
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(15 * time.Minute).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	return token, err
+}
+
+func GenerateRefreshToken() (string, error) {
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(20 * time.Minute).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	// Store the refresh token in the cache
+	RefreshCache.Set(token, nil, cache.DefaultExpiration)
+
+	return token, err
+}
+
+func ParseToken(token string) (jwt.MapClaims, error) {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("REFRESH_SECRET")), nil
+	})
+
+	return claims, err
+}
+
+func VerifyToken(tokenStr string) (bool, error) {
+	claims := jwt.MapClaims{}
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+
+	if err != nil {
+		return false, err
+	} else if !token.Valid {
+		return false, fmt.Errorf("Invalid token")
+	}
+
+	return true, nil
+}
